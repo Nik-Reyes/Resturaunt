60d7147850be70873b447e5b078c65afc702e585
“Nik <nikolaspreyes@gmail.com>
2025-06-11 18:05:27 -0700

    replace pie images

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,16 +1,16 @@
 import { createSpecials } from "../../components/Specials/special.js";
-import strawRubarb from "../../assets/images/strawRubarb.jpg";
+import strawRhubarb from "../../assets/images/strawRhubarb.jpg";
 import scac from "../../assets/images/saltedCaramelAppleCrumble.jpg";
 import keyLime from "../../assets/images/keyLime.jpg";
 
 export default function () {
   //each special will have a unique image and name, and number to create
   const images = {
-    strawRubarb,
+    strawRhubarb,
     scac,
     keyLime,
     mapNames: {
-      strawRubarb: "Stawberry Rubarb",
+      strawRhubarb: "Stawberry Rhubarb",
       scac: "Salted Caramel Apple Crumble",
       keyLime: "Key Lime",
     },
