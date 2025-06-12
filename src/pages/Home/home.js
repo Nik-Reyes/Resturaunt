f29538a76c353b97b448be99b418bbb729d5c2cd
“Nik <nikolaspreyes@gmail.com>
2025-06-11 17:03:38 -0700

    code: import and pass images to createSpecials module

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,8 +1,21 @@
-import specials from "../../components/Specials/special.js";
+import { createSpecials } from "../../components/Specials/special.js";
+import strawRubarb from "../../assets/images/strawRubarb.jpg";
+import scac from "../../assets/images/saltedCaramelAppleCrumble.jpg";
+import keyLime from "../../assets/images/keyLime.jpg";
 
 export default function () {
-  //call specials function to create the special items
-
-  specials();
-  console.log("here");
+  //each special will have a unique image and name, and number to create
+  const images = {
+    strawRubarb,
+    scac,
+    keyLime,
+    mapNames: {
+      strawRubarb: "Stawberry Rubarb",
+      scac: "Salted Caramel Apple Crumble",
+      keyLime: "Key Lime",
+    },
+  };
+  const monthlySpecials = createSpecials(images);
+  const content = document.querySelector(".content");
+  content.append(monthlySpecials);
 }
