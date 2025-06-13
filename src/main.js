0646e8377bda6892458a51642c67ec73735e286c
“Nik <nikolaspreyes@gmail.com>
2025-06-13 15:00:08 -0700

    feature: add ability to switch between menu and about pages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
 import "./style.css";
 import { home } from "./pages/Home/home.js";
-// import { menu } from "./pages/Menu/menu.js";
+import { menu } from "./pages/Menu/menu.js";
 
 home();
-// menu();
+
+const content = document.querySelector(".content");
+const homeButton = document.querySelector(
+  ".menu-buttons > .list-item:nth-child(1)"
+);
+const menuButton = document.querySelector(
+  ".menu-buttons > .list-item:nth-child(2)"
+);
+const aboutButton = document.querySelector(
+  ".menu-buttons > .list-item:nth-child(3)"
+);
+
+menuButton.addEventListener("click", async () => {
+  clearContent();
+  const { menu } = await import("./pages/Menu/menu.js");
+  menu();
+});
+
+homeButton.addEventListener("click", () => {
+  clearContent();
+  home();
+});
+
+const clearContent = () => {
+  content.innerText = "";
+};
