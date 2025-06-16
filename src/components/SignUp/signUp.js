eb5bbd4cda720c920c625ae1c08dc7727d37ab5a
“Nik <nikolaspreyes@gmail.com>
2025-06-16 15:31:45 -0700

    style signup form

--- src/components/SignUp/signUp.js
+++ src/components/SignUp/signUp.js
@@ -1,7 +1,9 @@
 import { utils } from "../../utils/Helpers/helpers.js";
+import signUpImage from "../../assets/images/signUpImage.jpg";
 import "./signUp.css";
 
 export function signup() {
+  const signupWrapper = utils.generateElement("div", "signup-wrapper");
   const title = utils.generateElement("div", "signup-title");
   const titleText = utils.generateElement(
     "p",
@@ -9,16 +11,46 @@ export function signup() {
     "Sign up for our tasty newsletter"
   );
 
-  title.appendChild(titleText);
-
   const imageWrapper = utils.generateElement("div", "nl-img-wrapper");
-  const img = generateElement("img", "news-img", newsLetterImage);
-  imageWrapper.appendChild(img);
+  const img = utils.generateElement("img", "news-img", signUpImage);
 
-  const newsletterSubtitle = utils.generateElement("div", "signup-title");
-  const newLetterSubText = utils.generateElement(
+  const emailSection = utils.generateElement("div", "email-section");
+  const newsletterSubtitle = utils.generateElement("div", "signup-subtitle");
+  const newsLetterSubText = utils.generateElement(
     "p",
     "nl-subtitle",
     "Sign up with your email address to recieve news and updates"
   );
+
+  const form = utils.generateElement("form", "signup-form");
+  const formControlWrapper = utils.generateElement(
+    "div",
+    "form-control-wrapper"
+  );
+  const mailFormControl = utils.generateElement("div", "form-control");
+  const label = utils.generateElement("label", "signup-label");
+  const labelTitle = utils.generateElement("span", "nl-label-title", "Email");
+  label.htmlFor = "mail";
+  const input = utils.generateElement("input", "signup-input");
+  input.type = "email";
+  input.name = "newsLetterSignup";
+  input.placeholder = "Email Address";
+  input.autocomplete = "email";
+
+  const btnFormControl = utils.generateElement("div", "form-control");
+  const submitButton = utils.generateElement("button", "signup-btn", "Sign Up");
+  submitButton.type = "submit";
+
+  label.append(labelTitle, input);
+  mailFormControl.append(label, input);
+  btnFormControl.appendChild(submitButton);
+  formControlWrapper.append(mailFormControl, btnFormControl);
+  form.appendChild(formControlWrapper);
+  newsletterSubtitle.appendChild(newsLetterSubText);
+  emailSection.append(newsletterSubtitle, form);
+  imageWrapper.appendChild(img);
+  title.appendChild(titleText);
+  signupWrapper.append(title, imageWrapper, emailSection);
+
+  return signupWrapper;
 }
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,11 +1,12 @@
 import { heroSection } from "../../components/Hero/hero.js";
+import { signup } from "../../components/SignUp/signUp.js";
 import { utils } from "../../utils/Helpers/helpers.js";
 import "./home.css";
 
 export function home() {
   const homeWrapper = utils.generateElement("div", "homepage-wrapper");
   const content = document.querySelector(".content");
-  homeWrapper.appendChild(heroSection());
+  homeWrapper.append(heroSection(), signup());
 
   content.appendChild(homeWrapper);
 }
