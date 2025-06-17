3e242c7af75ad1f8660a7adc81da0e2ddbdde900
“Nik <nikolaspreyes@gmail.com>
2025-06-16 20:38:48 -0700

    add basic routing

--- src/main.js
+++ src/main.js
@@ -1,33 +1,97 @@
-import "./style.css";
 import { home } from "./pages/Home/home.js";
-// import { menu } from "./pages/Menu/menu.js";
+import { menu } from "./pages/Menu/menu.js";
+import "./style.css";
 
-home();
+const pageHandler = (() => {
+  let specialsButton = null;
+  let previousPage = null;
+  let currentPage = "home";
+  const content = document.querySelector(".content");
+  const homeButton = document.querySelector(
+    ".menu-buttons > .list-item:nth-child(1)"
+  );
+  const menuButton = document.querySelector(
+    ".menu-buttons > .list-item:nth-child(2)"
+  );
 
-const content = document.querySelector(".content");
-const homeButton = document.querySelector(
-  ".menu-buttons > .list-item:nth-child(1)"
-);
-const menuButton = document.querySelector(
-  ".menu-buttons > .list-item:nth-child(2)"
-);
-const aboutButton = document.querySelector(
-  ".menu-buttons > .list-item:nth-child(3)"
-);
-const specialsButton = document.querySelector(".ad-button");
-specialsButton.addEventListener("click", () => {});
+  const clearContent = () => {
+    content.innerText = "";
+  };
 
-menuButton.addEventListener("click", async () => {
-  clearContent();
-  const { menu } = await import("./pages/Menu/menu.js");
-  menu();
-});
+  const assignEventListeners = (elements) => {
+    for (const value of Object.values(elements)) {
+      console.log(value);
+      if (previousPage === currentPage) {
+        continue;
+      } else {
+        console.log("add listener");
+        value.el.addEventListener(value.type, value.func);
+      }
+    }
+  };
 
-homeButton.addEventListener("click", () => {
-  clearContent();
-  home();
-});
+  const updatePage = (newPage) => {
+    previousPage = currentPage;
+    content.classList.remove(currentPage);
+    currentPage = newPage;
+    content.classList.add(currentPage);
 
-const clearContent = () => {
-  content.innerText = "";
-};
+    console.log(`previous page: ${previousPage}\ncurrent page: ${currentPage}`);
+  };
+
+  const clickMenu = () => {
+    menu();
+    updatePage("menu");
+  };
+
+  const clickHome = () => {
+    home();
+    updatePage("home");
+    specialsButton = document.querySelector(".ad-button");
+    if (previousPage !== currentPage) {
+      assignEventListeners({
+        special: {
+          el: specialsButton,
+          type: "click",
+          func: () => {
+            clearContent();
+            clickMenu();
+          },
+        },
+      });
+    }
+  };
+
+  const initialize = (() => {
+    // start homepage
+    home();
+    specialsButton = document.querySelector(".ad-button");
+
+    // set event listener states for all home buttons
+    assignEventListeners({
+      special: {
+        el: specialsButton,
+        type: "click",
+        func: () => {
+          clearContent();
+          clickMenu();
+        },
+      },
+    });
+
+    // apply home class
+    content.classList.add(currentPage);
+    // make the starting page (homepage) the previous page for conditionals
+    previousPage = currentPage;
+
+    menuButton.addEventListener("click", () => {
+      clearContent();
+      clickMenu();
+    });
+
+    homeButton.addEventListener("click", () => {
+      clearContent();
+      clickHome();
+    });
+  })();
+})();
