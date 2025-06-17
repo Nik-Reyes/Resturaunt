97a204aab7cd9d4ce0d32cc271bcd9ab41ab46e8
“Nik <nikolaspreyes@gmail.com>
2025-06-17 13:27:16 -0700

    build(npm) add prod/dev webpack configs and npm scripts

---
+++ webpack.dev.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    assetModuleFilename: (pathData) => {
+      return `assets/${pathData.filename.split("/").at(2)}/[name][ext]`;
+    },
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/index.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|webp|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
